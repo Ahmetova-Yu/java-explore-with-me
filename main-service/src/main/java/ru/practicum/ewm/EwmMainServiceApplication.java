df3aebc23faac323895778ca64149d33da8e9603
UAhmetova <uliaahm.com@yandex.ru>
2026-07-16 23:37:44 +0300

    Добавление модуля статистики и логики

--- main-service/src/main/java/ru/practicum/ewm/EwmMainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/ewm/EwmMainServiceApplication.java
@@ -1,13 +1,12 @@
-package ru.practicum.ewm.mainservice;
+package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainServiceApplication {
+public class EwmMainServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainServiceApplication.class, args);
+        SpringApplication.run(EwmMainServiceApplication.class, args);
     }
-
 }
