a2b35990087147f005a24c389a666b3b44d35dd5
UAhmetova <uliaahm.com@yandex.ru>
2026-07-17 13:18:07 +0300

    Правки

--- stats/stats-client/src/main/java/ru/practicum/ewm/stats/client/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/stats/client/StatsClient.java
@@ -17,7 +17,6 @@ import ru.practicum.ewm.stats.dto.ViewStatsDto;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class StatsClient {
