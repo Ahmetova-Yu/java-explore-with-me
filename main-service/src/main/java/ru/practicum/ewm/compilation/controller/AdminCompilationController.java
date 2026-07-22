d75600ad41c98f3104eaaec24c4ef27b45174b46
UAhmetova <uliaahm.com@yandex.ru>
2026-07-22 17:04:55 +0300

    Правки

--- main-service/src/main/java/ru/practicum/ewm/compilation/controller/AdminCompilationController.java
+++ main-service/src/main/java/ru/practicum/ewm/compilation/controller/AdminCompilationController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.compilation.dto.CompilationDto;
 import ru.practicum.ewm.compilation.dto.NewCompilationDto;
@@ -15,7 +14,6 @@ import ru.practicum.ewm.compilation.service.CompilationService;
 @RequestMapping("/admin/compilations")
 @RequiredArgsConstructor
 @Slf4j
-@Validated
 public class AdminCompilationController {
     private final CompilationService compilationService;
 
@@ -27,8 +25,9 @@ public class AdminCompilationController {
     }
 
     @PatchMapping("/{compId}")
-    public CompilationDto updateCompilation(@PathVariable Long compId,
-                                            @Valid @RequestBody UpdateCompilationRequest request) {
+    public CompilationDto updateCompilation(
+            @PathVariable Long compId,
+            @Valid @RequestBody UpdateCompilationRequest request) {
         log.info("PATCH /admin/compilations/{}", compId);
         return compilationService.updateCompilation(compId, request);
     }
